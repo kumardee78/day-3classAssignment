b747d652507194311d00cec033ffc868fa520c66
kumardee78 <deedancer951@gmail.com>
2024-05-29 12:54:10 +0530

    modified css

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
 function Cards() {
     return(
         <>
-            <section className="flex flex-wrap gap-8 py-14 px-8">
+            <section className="flex flex-wrap gap-8 pb-14 pt-6 px-8">
                 <div className="w-[30%]">
                     <p className="rounded-lg shadow-md bg-[#F9FAFB] py-6 px-6 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad inventore laudantium est illum voluptatem quis.</p>
                     <div className="flex ">
